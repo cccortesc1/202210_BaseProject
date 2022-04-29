332db4222111c351df9a29b4f2a567e124779e04
cccortesc1 <cc.cortesc1@uniandes.edu.co>
2022-04-29 01:01:44 -0500

    #2 se realiza segundo punto del parcial

--- src/app/coffee/coffee.component.ts
+++ src/app/coffee/coffee.component.ts
@@ -10,11 +10,43 @@ import { CoffeeService } from './coffee.service';
 export class CoffeeComponent implements OnInit {
 
   coffees: Array<Coffee> = [];
+  coffeesBlended: Array<Coffee> = [];
+  coffeesOrigin: Array<Coffee> = [];
+  blendedCoffeeCounter: number = 0;
+  originCoffeeCounter: number = 0;
 
-  constructor(private coffeeService: CoffeeService) { }
+  constructor(private coffeeService: CoffeeService) {
+
+  }
   getAllCoffees() {
     this.coffeeService.getAllCoffees()
       .subscribe(coffees => this.coffees = coffees);
+  }
+
+  getOriginCoffeeCounter(): number
+  {
+    var originCoffeeCounter = 0;
+
+    this.coffees.forEach(coff => {
+      if(coff.tipo != 'Blend'){
+        originCoffeeCounter++;
+      }
+    })
+
+    return originCoffeeCounter;
+  }
+
+  getBlendedCoffeeCounter(): number
+  {
+    var blendedCoffeeCounter = 0;
+
+    this.coffees.forEach(coff => {
+      if(coff.tipo == 'Blend'){
+        blendedCoffeeCounter+= 1;
+      }
+    })
+
+    return blendedCoffeeCounter;
   }
 
   ngOnInit() {
